349fb10548adf4a098f4d9ce56cf42ea61078be9
Zaphrog <diegocottin@gmail.com>
2019-07-07 23:33:17 +0000

    doddodododdo

--- example.py
+++ example.py
@@ -1,8 +1,8 @@
-import Neza_e
-import Neza_d
+import Neza
+import Neza
 example_message = "First arg is message second arg is passcode"
 example_passcode = 12345
-encrypted_message = Neza_e.encrypt(example_message, example_passcode)
+encrypted_message = Neza.encrypt(example_message, example_passcode)
 print(encrypted_message)
-decrypted_message = Neza_d.decrypt(encrypted_message, example_passcode)
+decrypted_message = Neza.decrypt(encrypted_message, example_passcode)
 print(decrypted_message)
