96cdec45b0f44e284511fdfcfe11fc9a49f39e28
Zaphrog <diegocottin@gmail.com>
2019-07-08 20:57:14 +0000

    done

--- Caesar.py
+++ Caesar.py
@@ -1,7 +1,19 @@
-
+import collections
 global alphabet
 alphabet = [" ","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X", "Y", "Z"]
 
+global languages
+languages = {
+    "ENGLISH":{
+        "most_used":" ETAOINSRHLDCUMFP"
+        },
+    "SPANISH":{
+        "most_used":"EAOSRNIDLCTUMPB"
+        },
+    "GERMAN":{
+        "most_used": "ENIRSTAHDULCGMO"
+        }
+    }
 
 def encrypt(message, encryption):
     message = message.upper()
@@ -26,27 +38,6 @@ def decrypt(new_message, encryption):
         old_message_decrypt = old_message_decrypt + alphabet[old_location]
     return old_message_decrypt
 
-
-
-import collections
-
-global alphabet
-alphabet = [" ","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X", "Y", "Z"]
-
-global languages
-languages = {
-    "ENGLISH":{
-        "most_used":" ETAOINSRHLDCUMFP"
-        },
-    "SPANISH":{
-        "most_used":"EAOSRNIDLCTUMPB"
-        },
-    "GERMAN":{
-        "most_used": "ENIRSTAHDULCGMO"
-        }
-    }
-
-
 def auto_decrypt(message, language, iterations):
     message = message.upper()
     language = language.upper()
@@ -69,12 +60,3 @@ def auto_decrypt(message, language, iterations):
                 old_location = old_location - 26
             old_message_auto = old_message_auto + alphabet[old_location]
         return old_message_auto
-
-
-
-enc = encrypt("Hello my name is diego pleased to meet you", 5)
-print(enc)
-dec = decrypt(enc, 5)
-print (dec)
-auto = auto_decrypt(enc, "english", 1)
-print(auto)
---
+++ example_caesar.py
@@ -0,0 +1,13 @@
+import Caesar
+
+message = "The arguments you pass are the message and a passcode no longer than twenty five"
+enc_mes = Caesar.encrypt(message, 5)
+#print(enc_mes)
+decrypted = Caesar.decrypt(enc_mes, 5)
+#print (decrypted)
+
+message_2 = "In the auto decrypt function you enter the language instead of the passcode plus the level you want to try"
+enc_mes = Caesar.encrypt(message, 5)
+print(enc_mes)
+auto = Caesar.auto_decrypt(enc_mes, "english", 1)
+print(auto)
