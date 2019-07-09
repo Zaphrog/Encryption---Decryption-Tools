0656b5ffba67418b6b2f5878c78e69acd2c020d9
Zaphrog <diegocottin@gmail.com>
2019-07-09 12:35:28 +0000

    Done

--- example_caesar.py
+++ example_caesar.py
@@ -2,12 +2,12 @@ import Caesar
 
 message = "The arguments you pass are the message and a passcode no longer than twenty five"
 enc_mes = Caesar.encrypt(message, 5)
-#print(enc_mes)
+print(enc_mes)
 decrypted = Caesar.decrypt(enc_mes, 5)
-#print (decrypted)
+print (decrypted)
 
 message_2 = "In the auto decrypt function you enter the language instead of the passcode plus the level you want to try"
-enc_mes = Caesar.encrypt(message, 5)
+enc_mes = Caesar.encrypt(message_2, 5)
 print(enc_mes)
 auto = Caesar.auto_decrypt(enc_mes, "english", 1)
 print(auto)
