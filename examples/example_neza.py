ee6027838d02e32a77c3c357a4d814e671cab7e5
Zaphrog <diegocottin@gmail.com>
2019-08-02 14:38:04 +0000

    Key fixed

--- examples/example_neza.py
+++ examples/example_neza.py
@@ -1,9 +1,9 @@
 import Neza
-
 example_message = "This just takes the message. You don't even need to input the password to decrypt it!"
-
-encrypted_message = Neza.encrypt(example_message)
+key = Neza.generate_key()
+print(key)
+encrypted_message = Neza.encrypt(example_message, key)
 print(encrypted_message)
 
-decrypted_message = Neza.decrypt(encrypted_message)
+decrypted_message = Neza.decrypt(encrypted_message, key)
 print(decrypted_message)
--- src/Neza.py
+++ src/Neza.py
@@ -1,12 +1,15 @@
 import random
 
-def encrypt(message):
-    message_list = []
-    message_int = 0
-    new_message = ""
-    for c in message:
-        message_list.append(ord(c))
+
+def generate_key():
     key = random.randint(1,256)
+    return key
+
+def encrypt(message, key):
+    message_list = []
+    message_int = 0
+    for c in message:
+        message_list.append(ord(c))
     temp_key = key
     for i in range(len(message_list)):
         message_list[i] = message_list[i] ^ temp_key
@@ -14,20 +17,15 @@ def encrypt(message):
     for item in message_list:
         message_int = message_int << 8
         message_int += item
-        new_message += chr(item)
-    message_int = message_int << 8
-    message_int += key
-    new_message += chr(key)
-    return new_message
+    return message_int
 
-def decrypt(message):
+def decrypt(message, key):
     message_list = []
     message_int = 0
     new_message = ""
-    for c in message:
-        message_list.append(ord(c))
-    key = message_list[-1]
-    message_list.pop()
+    while message > 0:
+        message_list.insert(0, (message % 2**8))
+        message = message >> 8
     temp_key = key
     for i in range(len(message_list)):
         message_list[i] = message_list[i] ^ temp_key
@@ -39,4 +37,3 @@ def decrypt(message):
     message_int = message_int << 8
     message_int += key
     return new_message
-
