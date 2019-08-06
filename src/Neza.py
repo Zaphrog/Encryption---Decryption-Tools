6fd31f7755e87d60ba88518242cf3983d07ee287
Zaphrog <diegocottin@gmail.com>
2019-08-06 15:00:40 +0000

    Implemented compression

--- src/Neza.py
+++ src/Neza.py
@@ -1,14 +1,19 @@
 import random
+import compress
 
 
 def generate_key():
     key = random.randint(1,256)
     return key
 
+
+
 def encrypt(message, key):
+    letters, compressed = compress.compress(message)
     message_list = []
     message_int = 0
-    for c in message:
+    encrypted = ""
+    for c in compressed:
         message_list.append(ord(c))
     temp_key = key
     for i in range(len(message_list)):
@@ -17,9 +22,10 @@ def encrypt(message, key):
     for item in message_list:
         message_int = message_int << 8
         message_int += item
-    return message_int
+        encrypted += chr(item)
+    return message_int, letters
 
-def decrypt(message, key):
+def decrypt(message, key, letters):
     message_list = []
     message_int = 0
     new_message = ""
@@ -34,6 +40,15 @@ def decrypt(message, key):
         message_int = message_int << 8
         message_int += item
         new_message += chr(item)
+    decompressed = compress.decompress(new_message, letters)
     message_int = message_int << 8
     message_int += key
-    return new_message
+    return decompressed
+
+
+message = "Testing...123"
+key = 97
+enc, letters = encrypt(message, key)
+print(enc)
+dec = decrypt(enc, key, letters)
+print(dec)
